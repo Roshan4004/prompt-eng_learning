3406087397fb77a2b054126f1f252d4bba23ea88
Roshan4004 <gautamroshan4004@gmail.com>
2024-05-29 00:39:11 +0545

    Added conditions for system context as well

---
+++ forth_level.py
@@ -0,0 +1,47 @@
+import json
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+from my_paraphraser import paraphrase_sentence
+
+# Original JSON data
+f = open('main_data.json')
+data=json.load(f)
+
+new_format_data = []
+
+def add_context_to_prompts(prompts):
+    if "beginner" in prompts:
+        sys_content = "You are an English learning tutor. This prompt is for beginner English learners so work accordingly and be nice."
+    elif "intermediate" in prompts:
+        sys_content = "You are an English learning tutor. This prompt is for intermediate English learners."
+    elif "advanced" in prompts:
+        sys_content = "You are an English learning tutor. This prompt is for advanced English learners. Make sure to ask them some questions as you're a good tutor."
+    else:
+        sys_content = "You are a really great and polite English learning tutor. You can even ask user's level in this field."
+    return sys_content
+
+
+for prompt in data:
+    for sample in prompt["samples"]:
+        sys_content=add_context_to_prompts(sample["input"])
+        para_imput=paraphrase_sentence(sample["input"])
+        new_entry1 = {
+            "messages": [
+                {"role": "system", "content": sys_content},
+                {"role": "user", "content": para_imput},
+                {"role": "assistant", "content": sample["output"]}
+            ]
+        }        
+        new_format_data.append(new_entry1)
+        new_entry2 = {
+            "messages": [
+                {"role": "system", "content": sys_content},
+                {"role": "user", "content": sample["input"]},
+                {"role": "assistant", "content": sample["output"]}
+            ]
+        }
+        new_format_data.append(new_entry2)
+
+with open("forth_level_data.jsonl", "w") as file:
+    for entry in new_format_data:
+        file.write(json.dumps(entry) + "\n")
